746d0feff76f2326e1baa6b2a46f8b528a6ffaf2
JJimenez71 <jordanjimenez510@gmail.com>
2024-02-28 20:25:44 -0700

    made login button dynamic

--- actio/LoginView.swift
+++ actio/LoginView.swift
@@ -9,16 +9,20 @@ import SwiftUI
 import Auth0
 
 struct LoginView: View {
-    @State private var loggedIn: Bool = false
+    @State var loggedIn: Bool = true
     var body: some View {
         VStack{
-            LoginButton()
-            LogoutButton()
+            if loggedIn{
+                LoginButton(loggedIn: $loggedIn)
+            } else {
+                LogoutButton(loggedIn: $loggedIn)
+            }
         }
     }
 }
 
 struct LoginButton: View {
+    @Binding var loggedIn: Bool
     var body: some View {
         Button(action: {
             Auth0
@@ -27,6 +31,7 @@ struct LoginButton: View {
                     switch result{
                     case .success(let credentials):
                         print("Obtained credentials: \(credentials)")
+                        loggedIn.toggle()
                     case .failure(let error):
                         print("Failed with: \(error)")
                     }
@@ -39,12 +44,14 @@ struct LoginButton: View {
 }
 
 struct LogoutButton: View {
+    @Binding var loggedIn: Bool
     var body: some View {
         Button(action: {
             Auth0.webAuth().clearSession { result in
                 switch result{
                 case .success:
                     print("logged out")
+                    loggedIn.toggle()
                 case .failure(let error):
                     print("Failed to log out: \(error)")
                 }
